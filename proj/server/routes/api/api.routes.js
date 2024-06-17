2517461d0d7282f0802f74cc857a750bce4cdf1c
Даниил Павленко <jias@MacBook-Air-Daniil.local>
2024-06-17 23:17:39 +0300

    0

--- proj/server/routes/api/api.routes.js
+++ proj/server/routes/api/api.routes.js
@@ -5,6 +5,9 @@ const bcrypt = require('bcrypt');
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
 const { User } = require('../../db/models');
+const { Tasks } = require('../../db/models');
+const { Theory } = require('../../db/models');
+
 
 const apiRouter = express.Router();
 
@@ -13,12 +16,67 @@ apiRouter.post('/api/registration', async (req, res) => {
     const currentuser = await User.findOne({ where: { email } })
     const hashedPassword = await bcrypt.hash(password1, 10)
 
-    if (!currentuser) {
+    if (!currentuser && password1 === password2) {
         const dbRes = await User.create({ username: nickname, email: email, password: hashedPassword, avatar: "default" })
         req.session.user_id = { id: dbRes.id, nick: dbRes.nickname }
         res.status(201).json(JSON.parse(JSON.stringify(dbRes)))
     } else {
         res.status(403).json({ message: "User already exists" })
+    }
+})
+
+apiRouter.post('/api/auth', async (req, res) => {
+    const { email, password } = req.body
+    const currentuser = await User.findOne({ where: { email } })
+    const passconnect = bcrypt.compare(password, currentuser.password);
+
+    if (currentuser && passconnect) {
+        const dbRes = await User.create({ username: nickname, email: email, password: hashedPassword, avatar: "default" })
+        req.session.user_id = { id: dbRes.id, nick: dbRes.nickname }
+        res.status(201).json(JSON.parse(JSON.stringify(dbRes)))
+    } else {
+        res.status(403).json({ message: "User already exists" })
+    }
+})
+
+apiRouter.get('/api/tasks', async (req, res) => {
+    const taskslist = await Tasks.findAll({});
+    if (taskslist) {
+        res.status(201).json(JSON.parse(taskslist))
+    } else {
+        res.status(403).json({ message: "No tasks for your filters" })
+    }
+})
+
+apiRouter.get('/api/theory', async (req, res) => {
+    const theorylist = await Theory.findAll({});
+    if (theorylist) {
+        res.status(201).json(JSON.parse(theorylist))
+    } else {
+        res.status(403).json({ message: "No theory for your filters" })
+    }
+})
+
+apiRouter.get('/api/profilestaticks', async (req, res) => {
+    const { id } = req.body;
+    const User = await User.findOne({ where: { id } });
+    if (User.jwt === req.session.cookie) {
+        res.status(201).json(JSON.parse(User))
+    } else {
+        res.status(403).json({ message: "No permission" })
+    }
+})
+
+apiRouter.get('/api/allusers', async (req, res) => {
+
+    const Userslist = await User.findAll({
+        attributes: ['nickname', 'jspoints', 'totalpoints']
+    })
+    if (Userslist) {
+        res.status(201).json(JSON.parse(User))
+    }
+    else {
+        res.status(403).json({ message: "No users or error" });
     }
 })
 
